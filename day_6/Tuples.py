cbebce89b46000a6504a914c62344073c9b8c350
Ramdan0505 <ramadan0555@icloud.com>
2025-07-14 23:58:53 -0400

    Update Day 6 Tuples.py with more exercises

--- day_6/Tuples.py
+++ day_6/Tuples.py
@@ -11,3 +11,13 @@ print(my_tuple)
 # Join brothers and sisters tuples and assign it to siblings
 siblings = my_tuple + ('Hala', 'Hola', 'Khalid')
 print(siblings)
+
+# How many siblings do you have?
+print(len(siblings))
+
+# Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+family_members = siblings + ('John Doe', 'Jane Doe')
+print(family_members)
+
+# Excercise Level 2
+# Unpack siblings and parents from family_members
