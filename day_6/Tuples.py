e4864f978ae68b522f6d41e413de48b30f764fc7
Ramdan0505 <ramadan0555@icloud.com>
2025-07-14 23:52:33 -0400

    add day 6 to 30 days of python

---
+++ day_6/Tuples.py
@@ -0,0 +1,13 @@
+# Day 6 of 30 days of Python
+
+# create an empty tuple
+my_tuple = ()
+print(my_tuple)
+
+# Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+my_tuple = ('Hala', 'Hola', 'Khalid')
+print(my_tuple)
+
+# Join brothers and sisters tuples and assign it to siblings
+siblings = my_tuple + ('Hala', 'Hola', 'Khalid')
+print(siblings)
